0e45a19e5ebaa5e7c2907e0126daf304e07a9eb6
Son Bui <ngocson.bui0312@gmail.com>
2025-04-14 13:54:24 +0300

    install tailwind and made flashcard app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import FlashCardApp from './page/flashcard/FlashCard';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="p-[20px] text-[20px]">My Apps</h1>
+      <FlashCardApp />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,4 @@
+/** @type {import('tailwindcss').Config} */
+export const darkMode = ["class"]
+export const content = ["src/**/*.{ts,tsx}", "src/components/**/*.{ts,tsx}", "src/pages/*.{ts,tsx}"]
+export const plugins = [require("tailwindcss-animate")]
