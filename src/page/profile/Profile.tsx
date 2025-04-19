ee0257c4657ed15addb4fcdf0a6b879d466fe19f
Son Bui <ngocson.bui0312@gmail.com>
2025-04-19 15:08:20 +0300

    Update Profile.tsx

--- src/page/profile/Profile.tsx
+++ src/page/profile/Profile.tsx
@@ -10,30 +10,31 @@ const Profile = () => {
             game enthusiast
           </div>
           <p className="py-2 border border-2 border-black italic">
-            who does not want to live forever?
+            "who does not want to live forever?"
           </p>
         </div>
       </div>
       <div className="w-3/10 p-8 text-justify border border-white border-2 italic text-sm bg-white text-black">
         <p>
-          Roads go ever ever on, Over rock and under tree, By caves where never
-          sun has shone, By streams that never find the sea; Over snow by winter
-          sown, And through the merry flowers of June, Over grass and over
-          stone, And under mountains in the moon. Roads go ever ever on, Under
-          cloud and under star. Yet feet that wandering have gone Turn at last
-          to home afar. Eyes that fire and sword have seen, And horror in the
-          halls of stone Look at last on meadows green, And trees and hills they
-          long have known. The Road goes ever on and on Down from the door where
-          it began. Now far ahead the Road has gone, And I must follow, if I
-          can, Pursuing it with eager feet, Until it joins some larger way,
-          Where many paths and errands meet. The Road goes ever on and on Down
-          from the door where it began. Now far ahead the Road has gone, And I
-          must follow, if I can, Pursuing it with weary feet, Until it joins
-          some larger way, Where many paths and errands meet. And whither then?
-          I cannot say. The Road goes ever on and on Out from the door where it
-          began. Now far ahead the Road has gone. Let others follow, if they
-          can! Let them a journey new begin. But I at last with weary feet Will
-          turn towards the lighted inn, My evening-rest and sleep to meet.”
+          The Road Goes Ever On Roads go ever ever on, Over rock and under tree,
+          By caves where never sun has shone, By streams that never find the
+          sea; Over snow by winter sown, And through the merry flowers of June,
+          Over grass and over stone, And under mountains in the moon. Roads go
+          ever ever on, Under cloud and under star. Yet feet that wandering have
+          gone Turn at last to home afar. Eyes that fire and sword have seen,
+          And horror in the halls of stone Look at last on meadows green, And
+          trees and hills they long have known. The Road goes ever on and on
+          Down from the door where it began. Now far ahead the Road has gone,
+          And I must follow, if I can, Pursuing it with eager feet, Until it
+          joins some larger way, Where many paths and errands meet. The Road
+          goes ever on and on Down from the door where it began. Now far ahead
+          the Road has gone, And I must follow, if I can, Pursuing it with weary
+          feet, Until it joins some larger way, Where many paths and errands
+          meet. And whither then? I cannot say. The Road goes ever on and on Out
+          from the door where it began. Now far ahead the Road has gone. Let
+          others follow, if they can! Let them a journey new begin. But I at
+          last with weary feet Will turn towards the lighted inn, My
+          evening-rest and sleep to meet.
           <br />
           <span className="font-bold">
             ― J.R.R. Tolkien, The Lord of the Rings
