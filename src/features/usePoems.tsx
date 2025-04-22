fb105c65660fc9ec7b5ab11d2db9f2d50312ebec
Son Bui <ngocson.bui0312@gmail.com>
2025-04-22 21:21:16 +0300

    update poem

---
+++ src/features/usePoems.tsx
@@ -0,0 +1,111 @@
+const poems = [
+  `Who does not want to live forever
+Cities of cinder, torn asunder
+Gone, the world I remember
+Dark, splintered, only past glimmer
+
+Reminiscence—missed dinners with lovers
+Warm embraces, touches of tender
+Your sights, your smiles, my solemn respite
+Shadows and echoes, lost, endless crevice
+
+Only fools seek to live forever
+Each wish ungranted, all prayers denied 
+Is there light? Where withered desires
+Still, storms cease, ere the sun rises
+
+From ember arise flames anew
+Sparks of thought, stitched unto view
+Ruins break, boughs stretch through slumber
+Be assured, like green blooms, we endure`,
+
+  `My prose so strong,
+ everything so verbose, you be in a comatose,
+ rhymes and poses, those are my only doses.
+My father overdoses, my mother adorned without roses,
+ my bros, my heroes, snorting them heroin,
+ seeking that morphine, ruining their foes with lines,
+ undo their lives, with rhymes so divine—there be only fratricide.
+Morning I stand in lines, night I shoot white lines.
+ At dawn I spit out lines, at dusk I line with my divine.
+ Everything is fine, as long as I have my lines.
+ No nice, all vice—one day I will split the divine, committing deicide.
+Like Moses splitting the sea, like Ernst splitting the atom,
+ I split my family—four doomed, five six feet under,
+ fathers, sons, and daughters,
+ blundered caskets lined with white powders.
+ Powerless, fatherless, tenderless—
+ my only salvation: thunders, white, revolver.
+Tonight shall I split, becoming the divine,
+ dictating fate, blessing all souls within my line of sight—
+ judge, jury, executioner—God be my guiding light.
+ For my only redemption: death of their wives,
+ committing homicide so horrid, they redefine baptize.
+Yet moment of truth, I cannot commit infanticide—
+ my soul burned by the innocence.
+ My hand runs hot like a fever, my eyes red in the mirror,
+ for now I am not God—only Lucifer.`,
+
+  `Today I am on stage
+ Colors on my face
+ Trying to dictate my fate
+
+But no matter what I say
+ They spew hate, vindicate, eviscerate
+ Unto trampled innocence, they masturbate
+
+Today I am on stage
+ Color is my face
+ Is it too late to change fate?
+Cadence my only weapon — sincerity my only option
+ Soundless scream in eternity
+ Frustration will never break me
+
+Today I stand on stage
+ All colors, my face
+ For the future, I am become fate — vindicate — eviscerate
+ Until there be no hate`,
+  `my line is fire, 
+enticed, prophesied, I deified whole humankind;
+my line so fine, 
+enlight all the civilized, defied fate against the clandestine;
+my line so bright,
+Shadow be long gone, never to be mourned;
+My line so divine,
+Mend worlds I torn, binds words I adorned, rise the horde of the unborn.
+
+For I am become god, of worlds long forlorn`,
+  `earthworm dirt born,
+ scorned by the sky,
+ lying on asphalt waiting to be reborn.
+
+Sea dog ocean brought,
+ black tar, scream unheard from afar,
+ funeral in a car.
+
+Red fox, prairie taught,
+ crimson coat, stack,
+ chirps, black, no answer back.
+
+Wingless children, yearning for the sky
+ brimstone, scorched, abandoned
+ barren—smoke, apparent
+ howling to heaven, only gasp`,
+  `Funeral zipped by WinRAR,
+Compressed, 128Mb, Mom.rar,
+Uncompressed, 50Mb, bill.excel,
+Wake.mp4 corrupted by a bell,
+Tales to sleep to.mp3,
+First words spoken.vid, 40 days late fee,
+Invitation.pdf, written in comic sans, edited by AI-chan.`,
+  `today I look out the window,
+routed crow,
+crownless crowd,
+childless sound
+blessed it be, no morrow
+`,
+];
+
+export const usePoems = () => {
+  return poems;
+};
--- src/page/poetry/Poetry.tsx
+++ src/page/poetry/Poetry.tsx
@@ -1,30 +1,30 @@
+import { useState } from 'react';
+import { usePoems } from '../../features/usePoems';
+
 const Poetry = () => {
+  const poems = usePoems();
+  const [currentPoem, setCurrentPoem] = useState(
+    poems[Math.floor(Math.random() * poems.length)]
+  );
+
+  const getRandomPoem = () => {
+    const randomIndex = Math.floor(Math.random() * poems.length);
+    setCurrentPoem(poems[randomIndex]);
+  };
+
   return (
     <div className="text-black bg-white h-full p-4 flex flex-col gap-2 justify-center items-start">
-      <p className="p-2 border border-2 border-black italic">
-        "who does not want to live forever?"
-      </p>
-      <pre className="whitespace-pre-wrap font-mono text-lg">
-        {`Who does not want to live forever
-Cities of cinder, torn asunder
-Gone, the world I remember
-Dark, splintered, only past glimmer
-
-Reminiscence—missed dinners with lovers
-Warm embraces, touches of tender
-Your sights, your smiles, my solemn respite
-Shadows and echoes, lost, endless crevice
-
-Only fools seek to live forever
-Each wish ungranted, all prayers denied 
-Is there light? Where withered desires
-Still, storms cease, ere the sun rises
-
-From ember arise flames anew
-Sparks of thought, stitched unto view
-Ruins break, boughs stretch through slumber
-Be assured, like green blooms, we endure`}
-      </pre>
+      <button
+        className="p-2 border border-2 border-black italic mb-4 cursor-pointer"
+        onClick={getRandomPoem}
+      >
+        Get a Different Poem
+      </button>
+      <div className="max-h-8/10 overflow-y-auto">
+        <pre className="whitespace-pre-wrap font-mono text-lg">
+          {currentPoem}
+        </pre>
+      </div>
     </div>
   );
 };
